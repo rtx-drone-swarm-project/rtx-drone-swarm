139e56ffa114963b2354208e500f94899eefef5e
Lawrence Tam <69015944+lawrence-tam@users.noreply.github.com>
2026-03-02 14:48:05 -0800

    Drone-33 Added lat long conversion
    Imported modules to help calculate lat, long and get altitude.

--- location.py
+++ location.py
@@ -1,5 +1,13 @@
 from scripts.swarm_command import connect_all, send_command
 from pymavlink import mavutil
+import requests
+import pyned2lla
+import math
+import numpy as np
+
+# Conversion constants
+D2R = math.pi / 180.0
+R2D = 180.0 / math.pi
 
 def get_drones_locations():
     """
@@ -16,14 +24,31 @@ def get_drones_locations():
 
         lat = msg.lat / 1e7
         lon = msg.lon / 1e7
-        alt = msg.relative_alt / 1000.0  # mm → meters
 
-        locations.append((lat, lon, alt))
+        locations.append((lat, lon))
 
-    return locations
+    return np.array(locations)
 
 def send_location(conn, north, east, down):
     """
     Move vehicle by a local NED offset (meters) once using COMMAND_LONG.
     """
     send_command(conn, mavutil.mavlink.MAV_CMD_DO_REPOSITION, p1=north, p2=east, p3=down)
+
+def get_elevation_open(lat, lon):
+    url = f"https://api.opentopodata.org/v1/srtm90m?locations={lat},{lon}"
+    r = requests.get(url).json()
+    return r["results"][0]["elevation"]
+
+if __name__ == "__main__":
+    # Define the origin point (reference location) in degrees
+    (lat0, lon0, alt0) = 44.532, -72.782, 1699
+
+    # Define the NED coordinates (relative to the origin) in meters
+    (north, east, down) = 1334.3, -2543.6, 359.64
+
+    # Perform the conversion (inputs for lat/lon must be radians)
+    (lat, lon, alt) = pyned2lla.ned2lla(lat0 * D2R, lon0 * D2R, alt0, north, east, down, pyned2lla.wgs84())
+
+    # Print results in degrees
+    print((lat * R2D, lon * R2D, alt))
